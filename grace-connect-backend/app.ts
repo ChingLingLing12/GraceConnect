7b26a64259a82b14312603685435b593662067ee
James Ling <james.ling2000@gmail.com>
2026-03-06 11:36:04 +0800

    cors

--- grace-connect-backend/app.ts
+++ grace-connect-backend/app.ts
@@ -13,7 +13,7 @@ const port = process.env.PORT || 4000;
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000',"https://grace-connect.vercel.app"],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000',"https://grace-connect.vercel.app","https://www.graceconnect.au"],
     credentials: true,
     optionsSuccessStatus: 200
 }));
