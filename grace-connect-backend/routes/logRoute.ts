38915dde5a2a1c121209a07aedf996293d25fdbf
James Ling <james.ling2000@gmail.com>
2026-03-16 16:34:40 +0800

    remove logs

--- grace-connect-backend/routes/logRoute.ts
+++ grace-connect-backend/routes/logRoute.ts
@@ -3,7 +3,6 @@ import logController  from '../controllers/logController';
 
 const router = express.Router();
 
-router.post('/logs', (req: any, res: any) => logController.createLog(req, res));
 router.get('/logs', (req: any, res: any) => logController.getLogs(req, res));
 
 export default router;
